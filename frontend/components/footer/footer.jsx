b99ce8176fefdcb0f7f8e6267d81ef984510a96e
Andrew Tae <andrewtae3368@gmail.com>
2016-11-09 18:52:22 -0800

    Reseed music player

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -14,17 +14,17 @@ class Footer extends React.Component {
   //Play button should stop and start this player from the track list
 
   render() {
-    // let song1 = {
-    //   url: 'https://s3-us-west-1.amazonaws.com/major9/5-21+%E4%B8%87%E8%8F%AF%E9%8F%A1.mp3',
-    //   displayText: 'Mangekyou by Jimmy Weckl'
-    // }
-    //
-    // let song2 = {
-    //   url: "https://s3-us-west-1.amazonaws.com/major9/4-31+Keiko+my+love.mp3",
-    //   displayText: "Keiko My Love by Jimmy Weckl"
-    // }
-    //
-    // let songs = [song1,song2]
+    let song1 = {
+      url: 'https://s3-us-west-1.amazonaws.com/major9/5-21+%E4%B8%87%E8%8F%AF%E9%8F%A1.mp3',
+      displayText: 'Mangekyou by Jimmy Weckl'
+    }
+
+    let song2 = {
+      url: "https://s3-us-west-1.amazonaws.com/major9/4-31+Keiko+my+love.mp3",
+      displayText: "Keiko My Love by Jimmy Weckl"
+    }
+
+    let songs = [song1,song2]
 
     return(
       <footer>
